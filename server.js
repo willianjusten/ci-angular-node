97aeb72807ba27ec912a6c4c9189d5526a88caa6
Willian Justen <willianjustenqui@gmail.com>
2015-12-10 23:34:17 -0200

    Creating first view

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 
 var app = express();
+app.set('views', __dirname)
 app.set('view engine', 'jade');
 
 app.get('*', function(req, res) {
     res.render('index');
 });
+
+app.listen(3000);
+console.log('Running at http://localhost:3000');
