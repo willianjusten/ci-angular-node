993a96c90456c65d434fc6fad45923b2984408f3
Willian Justen <willianjustenqui@gmail.com>
2015-12-26 03:27:54 -0200

    Change data to be equal to tutorial

--- jobs-data.js
+++ jobs-data.js
@@ -1,32 +1,35 @@
-var mongoose = require('mongoose');
-var Promise = require('bluebird');
-var jobsModel = require('./models/Job.js');
+var mongoose = require("mongoose");
+var Promise = require("bluebird");
+var jobModel = require('./models/Job');
 
-var Job = jobsModel.model;
-
-var jobs = [
-    {title:'Python Developer', description:'The best python developer from the world is needed.'},
-    {title:'Javascript Developer', description:'The best Javascript developer from the world is needed.'},
-    {title:'C# Developer', description:'The best C# developer from the world is needed.'},
-    {title:'C++ Developer', description:'The best C++ developer from the world is needed.'}
-];
-
-var createJob = Promise.promisify(Job.create, {context: Job});
+var Job = jobModel.model;
 
 var findJobs = function(query) {
     return Promise.cast(Job.find(query).exec());
-};
+}
+
+var createJob = Promise.promisify(Job.create, {context:Job});
+
+//exports
 
 exports.findJobs = findJobs;
 
 exports.connectDB = Promise.promisify(mongoose.connect, {context: mongoose});
 
 exports.seedJobs = function() {
-    return findJobs({}).then(function(collection) {
-        if (collection.length === 0) {
-            return Promise.map(jobs, function(job) {
+    return findJobs({}).then(function(collection){
+        if(collection.length === 0) {
+            return Promise.map(seedJobs, function(job){
                 return createJob(job);
             })
         }
     })
-};
+}
+
+var seedJobs = [
+    {title:'Cook', description:'You will be making bagels'},
+    {title:'Waiter', description:'You will be putting food on peoples tables'},
+    {title:'Programmer', description:'You will be mindlessly typing for hours'},
+    {title:'Axe Maker', description:'We need many axes made.. so many..'}
+];
+
