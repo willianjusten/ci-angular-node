ad4e4171443747678e3ae7986780c502dbc8f3bf
Willian Justen <willianjustenqui@gmail.com>
2015-12-26 01:15:38 -0200

    Refactoring test to add conection to before. Add tests to verify if job has a title and description.

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -18,14 +18,30 @@ function findJobs(query) {
 }
 
 describe('get jobs', function() {
-    it('should never be empty, since jobs are seeded', function(done) {
+
+    var jobs;
+
+    before(function(done) {
         connectDB(mongoAddress)
             .then(resetJobs)
             .then(jobModel.seedJobs)
             .then(findJobs)
-            .then(function(jobsList) {
-                    expect(jobsList.length).to.be.at.least(1);
-                    done();
-                });
+            .then(function(collection) {
+                jobs = collection;
+                done();
             });
+    });
+
+
+    it('should never be empty, since jobs are seeded', function() {
+        expect(jobs.length).to.be.at.least(1);
+    });
+
+    it('should have a job with a title', function() {
+        expect(jobs[0].title).to.not.be.empty;
+    });
+
+    it('should have a job with a description', function() {
+        expect(jobs[0].description).to.not.be.empty;
+    });
 });
